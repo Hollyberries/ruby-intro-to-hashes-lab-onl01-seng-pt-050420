4b6a0b39b56a3f26d01add3be9656c2edc29a63c
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-16 05:04:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,6 @@ def monopoly_with_fourth_tier
 		monopoly = {:railroads =>
 	            { 
 	              :pieces => 4,
-	              :names => {},
 	              :rent_in_dollars =>
 	              {
 	                :one_piece_owned => 25,
