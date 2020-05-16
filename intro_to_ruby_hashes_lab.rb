1a46e93725c0f478ccbcbbfcf53f37742865f8ee
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-16 04:57:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -70,8 +70,8 @@ def monopoly_with_fourth_tier
 	               {
 	                 :reading_railroad => {"mortgage value" => "$100"},
 	                 :pennsylvania_railroad => {"mortgage value" => "$200"},
-	                 :b_and_o_railroad => {},
-	                 :shortline => {}
+	                 :b_and_o_railroad => {"mortgage value" => "$400"},
+	                 :shortline => {"mortgage value" => "$800"}
 	              
 	               }
 	              
